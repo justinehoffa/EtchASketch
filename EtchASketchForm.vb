120b1896bec12a12fd243c266e6ddca9b6d82b98
Justine Hoffa <justinefarrer@yahoo.com>
2020-10-24 00:56:50 -0600

    Add project files.

---
+++ EtchASketchForm.vb
@@ -0,0 +1,15 @@
+﻿'Justine Hoffa
+'RCET0265
+'Fall2020
+'Etch-A-Sketch 
+'
+
+Option Explicit On
+Option Strict On
+Option Compare Binary
+
+Public Class EtchASketchForm
+    Private Sub EtchASketchForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
